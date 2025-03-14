947273967f5bad25066bef7bb355f91fad03d393
Seifu Tadesse <seifemichael12@outlook.com>
2025-03-14 23:57:32 +0300

    ajusting user routes with thier corsponding controllers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const userControllers = require('../controllers')
+const userControllers = require("../controllers/userControllers");
 
-router.get('/user', userControllers.welcomePage);
-router.post('/user/login', userControllers.userLogin);
-router.get('/user/profile', userControllers.userProfile);
-router.put('/user/profileUpdate', userControllers.updateUpdate);
-router.delete('/user/profileDelete', userControllers.updateDelete);
+router.get("/user", userControllers.welcomePage);
+router.post("/user/login", userControllers.userLogin);
+router.get("/user/profile", userControllers.userProfile);
+router.put("/user/updateProfile", userControllers.updateProfile);
+router.delete("/user/deleteProfile", userControllers.deleteProfile);
 
 module.exports = router;
